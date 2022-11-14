dfdf1f1abb45c76485e566ea676f15e36a3863b4
Pranto Dev <prantodev1@gmail.com>
2022-11-14 09:20:06 +0600

    two sum using two pointer

--- two_sum.py
+++ two_sum.py
@@ -10,4 +10,20 @@ def two_sum_finder(l: list, sum: int):
     return "ans not found"
 
 
-print(two_sum_finder([3, 5, -4, 8, 11, 1, -1, 6],10))
+def two_sum_pointer(l: list, target_sum: int):
+    l.sort()
+    left = 0
+    right = len(l) - 1
+    print(left,right)
+    while left < right:
+        temp = l[left] + l[right]
+        if temp == target_sum:
+            return [[l[left]], [l[right]]]
+        elif temp < target_sum:
+            left += 1
+        else:
+            right -= 1
+    return f"ans doesn't exist in the array : {l}"
+
+
+print(two_sum_pointer([3, 5, -4, 8, 11, 1, -1, 6], 13))
